b018fc2387341ef2b33e05942b398c1fee5eb1db
KK <k00kadikis@gmail.com>
2018-09-08 13:39:45 +0300

    Added function stubs and connection to postgres

--- server/main.go
+++ server/main.go
@@ -1,13 +1,70 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+const (
+	host   = "localhost"
+	port   = 5432
+	user   = "kadikis"
+	dbname = "bill_tracker"
+)
+
+func init() {
+	var psqlInfo string
+	psqlInfo = fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
+		host, port, user, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	router := gin.Default()
+
+	v1 := router.Group("/api/v1/bills")
+	{
+		v1.POST("/", postBill)
+		v1.GET("/", getBills)
+		v1.GET("/:id", getSingleBill)
+		v1.PUT("/:id", updateBill)
+		v1.DELETE("/:id", deleteBill)
+	}
+
+	router.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getBills(c *gin.Context) {
+
+}
+
+func getSingleBill(c *gin.Context) {
+
+}
+
+func postBill(c *gin.Context) {
+
+}
+
+func deleteBill(c *gin.Context) {
+
+}
+
+func updateBill(c *gin.Context) {
+
 }
